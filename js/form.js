d244d27d3190c1a80e282486863246f95a9289d2
Nazzarik <nazzarik@gmail.com>
2018-02-12 20:32:01 +0200

    ended project

--- js/form.js
+++ js/form.js
@@ -1,23 +1,35 @@
 var link = document.querySelector(".btn-serch");
 var form = document.querySelector(".popup-form");
 var dateArr = form.querySelector("[name=arrival]");
-var dateText = form.querySelector("[name=depart]");
+var datDep = form.querySelector("[name=depart]");
 
 form.classList.add("form-none");
 
-
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	console.log("Click to the button");
-	form.classList.remove("form-none");
-	dateArr.focus();
+	if (form.classList.contains("form-none")) {
+		form.classList.remove("modal-error");
+		form.classList.remove("form-none");
+		form.classList.add("modal-show");
+		dateArr.focus();
+	} else {
+		form.classList.add("form-none");
+	}
+});
+
+form.addEventListener("submit", function(evt) {
+	if (!dateArr.value || !datDep.value) {
+			evt.preventDefault();
+			form.classList.add("modal-error");
+			// console.log("error");
+		}
 });
 
 window.addEventListener("keydown", function(evt) {
 	if (evt.keyCode === 27 ) {
 		if(!form.classList.contains("form-none")) {
 			form.classList.add("form-none");
-			console.log("click");
 		}
 	}
 });
+
